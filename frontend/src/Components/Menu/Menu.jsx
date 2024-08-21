75c5e043c23b290bcedf12e48b90d783ec85aa3a
nguyenductrong1207 <131521216+nguyenductrong1207@users.noreply.github.com>
2024-08-21 11:46:27 +0700

    create directory and shop context

---
+++ frontend/src/Components/Menu/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Style.css";
+
+const Menu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Menu
---
+++ frontend/src/Context/ShopContext.jsx
@@ -0,0 +1,122 @@
+// import React, { createContext, useEffect, useState } from "react";
+// import config from "../config";
+
+// export const ShopContext = createContext(null);
+
+// const getDefaultCart = () => {
+//   let cart = {};
+//   for (let i = 0; i < 300 + 1; i++) {
+//     cart[i] = 0;
+//   }
+//   return cart;
+// };
+
+// const ShopContextProvider = (props) => {
+//   // BaseURL
+//   const url = config.url;
+
+//   const [allBooks, setAllBooks] = useState([]);
+//   const [cartItems, setCartItems] = useState(getDefaultCart());
+
+//   useEffect(() => {
+//     fetch(url + "/getAllBooks")
+//       .then((res) => res.json())
+//       .then((data) => setAllBooks(data));
+
+//     if (localStorage.getItem("auth-token")) {
+//       fetch(url + "/getCart", {
+//         method: "POST",
+//         headers: {
+//           Accept: "application/form-data",
+//           "auth-token": `${localStorage.getItem("auth-token")}`,
+//           "Content-Type": "application/json",
+//         },
+//         body: "",
+//       })
+//         .then((res) => res.json())
+//         .then((data) => setCartItems(data));
+//     }
+//   }, []);
+
+//   const addToCart = (itemId) => {
+//     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+//     if (localStorage.getItem("auth-token")) {
+//       fetch(url + "/addToCart", {
+//         method: "POST",
+//         headers: {
+//           Accept: "application/form-data",
+//           "auth-token": `${localStorage.getItem("auth-token")}`,
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ itemId: itemId }),
+//       })
+//         .then((res) => res.json())
+//         .then((data) => console.log(data));
+//     }
+//   };
+
+//   const removeFromCart = (itemId) => {
+//     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+
+//     if (localStorage.getItem("auth-token")) {
+//       fetch(url + "/deleteFromCart", {
+//         method: "POST",
+//         headers: {
+//           Accept: "application/form-data",
+//           "auth-token": `${localStorage.getItem("auth-token")}`,
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ itemId: itemId }),
+//       })
+//         .then((res) => res.json())
+//         .then((data) => console.log(data));
+//     }
+//   };
+
+//   const getTotalCartAmount = () => {
+//     let totalAmount = 0;
+
+//     allBooks.map((book) => {
+//       if (cartItems[book.id] > 0) {
+//         totalAmount += book.salePrice * cartItems[book.id];
+//       }
+//     });
+
+//     // for (const item in cartItems) {
+//     //   if (cartItems[item] > 0) {
+//     // let bookInfo = allBooks.find((book) => book.id === Number(item));
+//     // totalAmount += bookInfo.salePrice * cartItems[item];
+//     //   }
+//     // }
+
+//     return totalAmount;
+//   };
+
+//   const getTotalCartItems = () => {
+//     let totalItems = 0;
+//     for (const item in cartItems) {
+//       if (cartItems[item] > 0) {
+//         totalItems += cartItems[item];
+//       }
+//     }
+
+//     return totalItems;
+//   };
+
+//   const contextValue = {
+//     getTotalCartItems,
+//     getTotalCartAmount,
+//     allBooks,
+//     cartItems,
+//     addToCart,
+//     removeFromCart,
+//   };
+
+//   return (
+//     <ShopContext.Provider value={contextValue}>
+//       {props.children}
+//     </ShopContext.Provider>
+//   );
+// };
+
+// export default ShopContextProvider;
